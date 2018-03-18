e2a9726a7f66c5217538aed931c1fb2391edbfa4
nwu <nwu@mit.edu>
2018-03-18 04:13:37 -0400

    got rid of stupid preloaders

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import Loader from './Loader';
 import WebFont from 'webfontloader';
 import {Switch, Route} from 'react-router-dom';
 import {fontList} from './fonts';
-import {allImages} from './images';
-import {withImagesPromise} from 'react-images-preload';
 import {mainPaths} from "./Router";
 import "./styles.css"
+import {allImages} from "./images";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true,
+      loadingFont: true,
       show: false
     };
-    this.finishLoading = this.finishLoading.bind(this);
+    this.finishLoadingFont = this.finishLoadingFont.bind(this);
+    this.finishLoadingComponent = this.finishLoadingComponent.bind(this);
     this.hideLoader = this.hideLoader.bind(this);
   }
 
-  finishLoading() {
-    this.setState({loading: false})
+  finishLoadingFont() {
+    this.setState({loadingFont: false})
+  }
+
+  finishLoadingComponent() {
+    this.setState(this.setState((state) => ({loadingComp: state.loadingComp - 1})))
   }
 
   hideLoader() {
@@ -30,8 +34,8 @@ class App extends Component {
   componentWillMount() {
     WebFont.load({
       google: fontList,
-      active: this.finishLoading,
-      inactive: this.finishLoading
+      active: this.finishLoadingFont,
+      inactive: this.finishLoadingFont
     });
   }
 
@@ -40,13 +44,13 @@ class App extends Component {
       <Route render={({location}) => {
         return (
           <div>
-            <Loader show={this.state.loading || !this.props.isImagesLoaded} welcome={true} finish={this.hideLoader}/>
+            <Loader show={this.state.loadingFont || this.props.loadingImages} welcome={true} finish={this.hideLoader}/>
             <Switch location={location}>
               {mainPaths.map((route, idx) => {
                 let Comp = route.component;
                 return (
                   <Route key={idx} exact={route.exact} path={route.path}>
-                    <Comp images={this.props.images} show={this.state.show}/>
+                    <Comp images={this.props.images} show={this.state.show} loaded={this.finishLoadingComponent}/>
                   </Route>
                 )
               })}
@@ -58,4 +62,30 @@ class App extends Component {
   }
 }
 
-export default withImagesPromise(allImages, App);
+const imageNames = Object.keys(allImages);
+
+class Wrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: imageNames.length,
+      images: allImages
+    }
+  }
+
+  componentDidMount() {
+    for (let i = 0; i < imageNames.length; i++) {
+      let newImage = new Image(0,0);
+      newImage.onload = () => {this.setState((state)=>({loading: state.loading - 1}))};
+      newImage.src = allImages[imageNames[i]];
+    }
+  }
+
+  render() {
+    return (
+      <App {...this.props} loadingImages={this.state.loading > 0} images={this.state.images}/>
+    )
+  }
+}
+
+export default Wrapper;
